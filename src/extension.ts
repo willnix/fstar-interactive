25fe57db1ead1ce0e4ec7063a300a3c9d44d2244
willnix <frieder.steinmetz@gmail.com>
2017-01-11 02:55:30 +0100

    complete rewrite

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-var spawnCMD = require('spawn-command');
 
-var process = null;
-var fstarOutput = null;
-var fstarInput = null;
-var fstarLastCheck = new vscode.Position(0,0)
+import * as vscode from 'vscode';
+const spawn = require('child_process').spawn;
+const dirname = require('path').dirname;
+const endOfLine = require('os').EOL;
+
+let fstarProcessList = {};
+let currentBlockStartList = {};
+let codeBlocks: Array<string>;
+let currentBlockList = {};
 
 const verifiedCodeDecorationType = vscode.window.createTextEditorDecorationType({
         // verified code will be violet
@@ -20,112 +21,183 @@ const erroneousLineDecorationType = vscode.window.createTextEditorDecorationType
 	});
     
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // Spawn the fstar process
-    process = spawnCMD("fstar.exe --in");
+    // verify next block
+    let disposableNextBlock = vscode.commands.registerCommand('extension.verifyNextBlock', () => {
+        let editor  = vscode.window.activeTextEditor;
+        let fileUri = editor.document.uri.toString()
+        let fstarProcess = fstarProcessList[fileUri]
+        let currentBlock = currentBlockList[fileUri]
+        let currentBlockStart = currentBlockStartList[fileUri]
+        // make sure everything is initialized
+        if(typeof currentBlock === 'undefined') {
+            currentBlock = 0;
+            currentBlockList[fileUri] = currentBlock;
+        }
+        if(typeof currentBlockStart === 'undefined') {
+            currentBlockStart = 0;
+            currentBlockStartList[fileUri] = currentBlockStart;
+        }
+        // spawn the fstar process if it is not running
+        if(typeof fstarProcess === 'undefined') {
+            fstarProcess = spawn("fstar.exe --in", { cwd: dirname(vscode.window.activeTextEditor.document.fileName), shell: true});
+            fstarProcessList[fileUri] = fstarProcess
 
-    // This handles all output from the fstar process and displays is as info msg
-    function printOutput(data) { 
-     var editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            return; // No open text editor
+            fstarProcess.stdout.on('data', (data) => {
+                processBlockResponse(data);
+                // done with current block
+                // next block starts 2 lines after the current one
+                currentBlockStart += codeBlocks[currentBlock].split(endOfLine).length+2;
+                currentBlockStartList[fileUri] = currentBlockStart;
+                currentBlock++;
+                currentBlockList[fileUri] = currentBlock;
+                if(currentBlock>=codeBlocks.length) {
+                    currentBlock = 0;
+                    fstarProcess.kill()
+                    delete fstarProcessList[fileUri]
+                }
+            });
         }
 
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
-
-        // give visual feedback
-        var response = data.toString();
-        if (response.includes("#done-ok")) {
-            // remove error decorations
-            const erroneousLine: vscode.DecorationOptions[] = [];
-            var docStart = new vscode.Position(0,0)
-            const decorationOptEL = {
-                range: new vscode.Range(docStart,docStart),
-                hoverMessage: ""}
-            erroneousLine.push(decorationOptEL);
-            editor.setDecorations(erroneousLineDecorationType,erroneousLine);
-
-            console.log('FStar: Verified OK');
-            vscode.window.showInformationMessage('FStar: Veryfied OK');
-            // set bg color
-            const verifiedCode: vscode.DecorationOptions[] = [];
-            const decorationOpt = {
-                range: new vscode.Range(new vscode.Position(0,0),fstarLastCheck),
-                hoverMessage: "Verified"}
-            verifiedCode.push(decorationOpt);
-            editor.setDecorations(verifiedCodeDecorationType,verifiedCode);
-    } else {
-            // remove bg color
-            const verifiedCode: vscode.DecorationOptions[] = [];
-            const decorationOptVC = {
-                range: new vscode.Range(new vscode.Position(0,0),new vscode.Position(0,0)),
-                hoverMessage: ""}
-            verifiedCode.push(decorationOptVC);
-            editor.setDecorations(verifiedCodeDecorationType,verifiedCode);
-            
-            // find erroneous line
-            const regex = /\<input\>\((\d+),(\d+)-(\d+),(\d+)\)\:\s(.*)/;
-            var errorTokens = regex.exec(response)
-
-            if (errorTokens == null) {
-                console.log('Error: Could not parse fstar response: ' + response);
-                vscode.window.showInformationMessage('Error: Could not parse fstar response: ' + response);
-                return                
-            }
-
-            // decorate erroneous line
-            const erroneousLine: vscode.DecorationOptions[] = [];
-            var errStart = new vscode.Position(parseInt(errorTokens[1])-1,parseInt(errorTokens[2]))
-            var errStop = new vscode.Position(parseInt(errorTokens[3])-1,parseInt(errorTokens[4]))
-            const decorationOptEL = {
-                range: new vscode.Range(errStart,errStop),
-                hoverMessage: errorTokens[5]}
-            erroneousLine.push(decorationOptEL);
-            editor.setDecorations(erroneousLineDecorationType,erroneousLine);
-
-            console.log('FStar error in Line '+errorTokens[1]+': '+errorTokens[5]);
-            vscode.window.showInformationMessage('FStar error in Line '+errorTokens[1]+': '+errorTokens[5]);
-        }
-           
-    }
-    process.stdout.on('data', printOutput);
-    process.stderr.on('data', printOutput);
-
-    // Verify the complete document
-    let disposableComplete = vscode.commands.registerCommand('extension.verifyComplete', () => {
-        // Get the code
-        var code = vscode.window.activeTextEditor.document.getText();
-
-        process.stdin.write("#push\n");
-        process.stdin.write(code);
-        process.stdin.write("\n#end #done-ok #done-nok\n");
-        process.stdin.write("#pop\n");
-    });
+        let code = editor.document.getText();
+        // split code in blocks
+        codeBlocks = code.split(endOfLine+endOfLine+endOfLine);
+        // write next block
+        fstarProcess.stdin.write("#push ${codeBlockStart}\n");
+        fstarProcess.stdin.write(codeBlocks[currentBlock]);
+        fstarProcess.stdin.write("\n#end #done-ok #done-nok\n");
+       
+    })
 
     // Verify until cursor
     let disposableToCursor = vscode.commands.registerCommand('extension.verifyToCursor', () => {
+        // Spawn a seperate fstar process
+       let fstarProcess = spawn("fstar.exe --in", { cwd: dirname(vscode.window.activeTextEditor.document.fileName), shell: true});
+       let currentBlock = 0;
+       let currentBlockStart = 0;
+
         // Get the cursor position
-        var editor  = vscode.window.activeTextEditor;
-        var cursorPos = editor.selection.active;
-        var range = new vscode.Range(new vscode.Position(0,0),cursorPos);
-        var code = editor.document.getText(range);
+        let editor  = vscode.window.activeTextEditor;
+        let cursorPos = editor.selection.active;
+        let range = new vscode.Range(new vscode.Position(0,0),cursorPos);
+        let code = editor.document.getText(range);
 
-        // Save current position
-        fstarLastCheck = cursorPos;
+        // Split code in blocks
+        codeBlocks = code.split(endOfLine+endOfLine+endOfLine);
+        currentBlock = 0;
+        // Write first block
+        fstarProcess.stdin.write("#push\n");
+        fstarProcess.stdin.write(codeBlocks[currentBlock]);
+        fstarProcess.stdin.write("\n#end #done-ok #done-nok\n");
 
-        process.stdin.write("#push\n");
-        process.stdin.write(code);
-        process.stdin.write("\n#end #done-ok #done-nok\n");
-        process.stdin.write("#pop\n");
+        fstarProcess.stdout.on('data', (data) => {
+            processBlockResponse(data);
+            // done with current block
+            // next block starts 2 lines after the current one
+            currentBlockStart += codeBlocks[currentBlock].split(endOfLine).length+2;
+            currentBlock++;
+
+            nextBlock();
+        });
+
+        // nextBlock() writes a block to the fstar process
+        function nextBlock() {
+            if(currentBlock<codeBlocks.length) {
+                fstarProcess.stdin.write("#push\n");
+                fstarProcess.stdin.write(codeBlocks[currentBlock]);
+                // tell fstar we're done
+                fstarProcess.stdin.write("\n#end #done-ok #done-nok\n");
+            } else {
+                fstarProcess.kill()
+            }
+        }
+
     });
 
-    context.subscriptions.push(disposableComplete);
+    context.subscriptions.push(disposableNextBlock);
     context.subscriptions.push(disposableToCursor);
+}
+
+// This handles all output from the fstar process
+function processBlockResponse(data: string) :boolean { 
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return false; // No open text editor
+    }
+    let fileUri = editor.document.uri.toString()
+    let currentBlock = currentBlockList[fileUri]
+    let currentBlockStart = currentBlockStartList[fileUri]
+
+    let response = data.toString();
+    console.log(response)
+
+    if (response.includes("#done-ok")) {
+        // remove error decorations (there must be a better way)
+        const erroneousLine: vscode.DecorationOptions[] = [];
+        let docStart = new vscode.Position(0,0)
+        const decorationOptEL = {
+            range: new vscode.Range(docStart,docStart),
+            hoverMessage: ""}
+        erroneousLine.push(decorationOptEL);
+        editor.setDecorations(erroneousLineDecorationType,erroneousLine);
+
+        // inform the user of the success
+        console.log('FStar: Verified OK');
+        vscode.window.showInformationMessage('FStar: Veryfied OK');
+
+        const verifiedCode: vscode.DecorationOptions[] = [];
+        // define the block to be marked
+        let blockEnd = new vscode.Position(currentBlockStart+codeBlocks[currentBlock].split(endOfLine).length,0);
+        const decorationOpt = {
+            range: new vscode.Range(docStart, blockEnd),
+            hoverMessage: "Verified"}
+        // apply the decoration
+        verifiedCode.push(decorationOpt);
+        editor.setDecorations(verifiedCodeDecorationType,verifiedCode);
+
+        return true;
+    } else {
+        // remove bg color (there must be a better way)
+        const verifiedCode: vscode.DecorationOptions[] = [];
+        const decorationOptVC = {
+            range: new vscode.Range(new vscode.Position(0,0),new vscode.Position(currentBlockStart-2,0)),
+            hoverMessage: ""}
+        verifiedCode.push(decorationOptVC);
+        editor.setDecorations(verifiedCodeDecorationType,verifiedCode);
+
+        // extract error position from fstars response
+        const regex = /\<input\>\((\d+),(\d+)-(\d+),(\d+)\)\:\s(.*)/;
+        let errorTokens = regex.exec(response)
+        if (errorTokens == null) {
+            console.log('Error: Could not parse fstar response: ' + response);
+            vscode.window.showInformationMessage('Error: Could not parse fstar response: ' + response);
+            return false;                
+        }
+
+        // inform the user of the error
+        console.log('FStar error in Line '+errorTokens[1]+': '+errorTokens[5]);
+        vscode.window.showInformationMessage('FStar error in Line '+errorTokens[1]+': '+errorTokens[5]);
+        
+        // decorate erroneous area
+        const erroneousLine: vscode.DecorationOptions[] = [];
+        // define the area to be marked according to fstars output
+        let errStart = new vscode.Position(parseInt(errorTokens[1])+currentBlockStart-1,parseInt(errorTokens[2]))
+        let errStop = new vscode.Position(parseInt(errorTokens[3])+currentBlockStart-1,parseInt(errorTokens[4]))
+        const decorationOptEL = {
+            range: new vscode.Range(errStart,errStop),
+            hoverMessage: errorTokens[5]}
+        // apply the decoration
+        erroneousLine.push(decorationOptEL);
+        editor.setDecorations(erroneousLineDecorationType,erroneousLine);
+
+        return false;
+    }
+        
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    Object.keys(fstarProcessList).forEach((key) => {
+        fstarProcessList[key].kill()
+    })
 }
